fb618fca0a1a347c9c1f5f419604b356aa17ff60
Yash Pandey <yashdevesh10@gmail.com>
2022-08-01 01:31:33 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -23,15 +23,19 @@ const App = () => {
     return(
         <>
             <div className='bg'>
-                <div className='container card a'>
+                <div className='container card p-3 mb-5 rounded a'>
                     <h1 className='heading'>ToDo List</h1>
                 
                     <form onSubmit={submit}>
-                       <div className='input' style={{float:'left'}}>
+                       <div className='input ' style={{float:'left'}}>
                             <input type='text' placeholder='Enter Items to add' className="form-control" onChange={inputevent} value={inputvalue} required/>
                         </div>
                         <div className='submitbtn'>
-                            <button type="submit" className="btn btn-primary active" style={{width:'13%'}}> + </button>
+                            <button type="submit" className="btn btn-primary" style={{padding:'5px 8px 3px 8px'}}>
+                                <span className="material-symbols-outlined">
+                                    add
+                                </span>
+                            </button>
                         </div>
                     </form><br/>
 
@@ -41,11 +45,22 @@ const App = () => {
                             {addedvalue.map((val ,index) => {
                                     return(
                                         <>
-                                            <List listvalue={val} showindex={index+1} />
+                                            <List 
+                                            key={index}
+                                            listvalue={val}
+                                            showindex={index+1} />
                                         </>
                                     );
                                 }
                             )}
+
+                            {/* <li className="list-group-item list-group-flush"><span>Apple</span>
+                                <button className="material-symbols-outlined btn btn-primary button" style={{float:'right'}}>Delete</button>
+                            </li>
+                            <li className="list-group-item list-group-flush"><span>Apple</span>
+                                <button className="material-symbols-outlined btn btn-primary button" style={{float:'right'}}>Delete</button></li>
+
+                            <li className="list-group-item list-group-flush"><span>Applesadsadasdsdaasddasdsaasddasadsdasdasdsad</span><button className="material-symbols-outlined btn btn-primary button" style={{float:'right'}}>Delete</button></li> */}
                             
                         </ol>
                     </div>
--- src/List.jsx
+++ src/List.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
 const List = (props) => {
+    
+    const deletebtn = () => {
+        console.log('deleted');
+    }
+
     return (
         <>
             
-            <li className="list-group-item list-group-item-action">
-                <span className='listindex'>{props.showindex}.</span>
-                {props.listvalue}
+            <li className="list-group-item list-group-item-action" >
+                <div className='row'>
+                    <div className='col-2'>
+                        <span className='listindex'>{props.showindex}.</span>
+                    </div>
+                    
+                    <div className='col-8'>
+                        <span>{props.listvalue}</span>
+                    </div>
+                    
+                    
+                    <div className='col-2'> 
+                        <button className="material-symbols-outlined btn btn-primary" style={{padding:'8px'}} onClick={deletebtn}>delete</button>  
+                    </div>
+                    
+                </div>  
             </li>
         </>
     );
