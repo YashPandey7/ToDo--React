780f960dcd0becb98d608d222d416b65ddb14274
Yash Pandey <yashdevesh10@gmail.com>
2022-07-31 21:00:28 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
 
+    const [inputvalue , setinput] = useState('');
+    const [addedvalue , setaddedvalue] = useState('');
+    
+    const inputevent = (event) =>{
+        const value = event.target.value;
+
+        setinput(value);
+    }
+
     const submit = (event) => {
         event.preventDefault();
+        setaddedvalue(inputvalue);
     }
 
     return(
@@ -11,19 +21,27 @@ const App = () => {
             <div className='bg'>
                 <div className='container card a'>
                     <h1 className='heading'>ToDo List</h1>
-                <form onSubmit={submit}>
-                <div className="row" >
-                    <div className='col-md-8'>
-                    <input type='text' placeholder='Enter Items to add' class="form-control"/>
+                
+                    <form onSubmit={submit}>
+                       <div className='input' style={{float:'left'}}>
+                            <input type='text' placeholder='Enter Items to add' class="form-control" onChange={inputevent} value={inputvalue}/>
+                        </div>
+                        <div className='submitbtn'>
+                            <button type="submit" class="btn btn-primary active" style={{width:'13%'}}> + </button>
+                        </div>
+                    </form><br/>
+
+                    <div className='list'>
+                        <ol className="list-group list-group-flush">
+                            <li className="list-group-item list-group-item-action">{addedvalue}</li>
+                            
+                            
+                            {/* <li className="list-group-item list-group-item-action">orange</li>
+                            <li className="list-group-item list-group-item-action">orange</li>
+                            <li className="list-group-item list-group-item-action">orange</li> */}
+                        </ol>
                     </div>
-                    <div className='col-md-4'>
-                    <button type="submit" class="btn btn-primary active">+</button>
-                    </div>
-                </div>
-                </form>
-                    <ol>
-                        <li>Apple</li>
-                    </ol>
+                    
                 </div>
             </div>
         </>
